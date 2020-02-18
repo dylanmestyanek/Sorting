66731d810c288292c2a6995e4289bcc7dabb48cc
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-02-17 21:05:45 -0600

    Redid a selection sort without looking at example

--- src/iterative_sorting/without_looking.py
+++ src/iterative_sorting/without_looking.py
@@ -5,6 +5,19 @@ def bubblesort(arr):
                 arr[i], arr[j] = arr[j], arr[i]
     return arr
 
-print(bubblesort([1,3,2,7,9]))
+# print(bubblesort([1,3,2,7,9]))
 
-def 
+def selection_sort(arr):
+    for i in range(len(arr)):
+        smallest = i
+
+        for j in range(i, len(arr)):
+            if arr[j] < arr[i]:
+                smallest = j
+        
+        arr[smallest], arr[i] = arr[i], arr[smallest]
+
+    return arr
+
+
+print(selection_sort([1,3,2,8,7,9, 11, 10, 8231, 394, 4]))
