494ca3c8c763456132109b38ee47fc6cb9f0eef3
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-02-17 21:00:00 -0600

    Redid a bubblesort function without looking at example.

---
+++ src/iterative_sorting/without_looking.py
@@ -0,0 +1,10 @@
+def bubblesort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[i] < arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
+print(bubblesort([1,3,2,7,9]))
+
+def 
